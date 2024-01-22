164803ba5570d5a6919e78ff8c126c46a83c4615
tom <tomchenforwork@gmail.com>
2024-01-22 12:26:46 +0800

    router setting & delete example code except useState code

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './Style/index.scss'
 import App from './App.tsx'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import './Style/index.scss'
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([{
     path: "/",
     element: <App />,
+    children: [{
+        path: "/",
+        element: <div>home</div>,
+    }, {
+        path: "/about",
+        element: <div>about</div>,
+    }],
+}, {
+    path: "*",
+    element: <Navigate to={"/"} />
 }]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
