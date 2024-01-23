3c767eee60c672a96c237c0dba1813e980aab144
tom <tomchenforwork@gmail.com>
2024-01-24 01:09:58 +0800

    init shadcn-ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import "./style/index.scss"
+import "@/style/index.scss"
 import {RouterProvider} from "react-router-dom";
-import router from "./routerConfig.tsx";
+import router from "@/routerConfig.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
